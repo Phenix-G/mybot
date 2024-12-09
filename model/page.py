7c58db2724ff3993558a58692611fc851868481e
Phenix-G <eiphenixg@gmail.com>
2024-12-09 22:08:46 +0800

    update model

--- model/page.py
+++ model/page.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from sqlmodel import SQLModel, Field
 from sqlalchemy import TEXT
 
@@ -5,5 +7,5 @@ from sqlalchemy import TEXT
 class Page(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
-    content: str = Field(sa_type=TEXT)
-
+    url: Optional[str] = None
+    content: str = Field(sa_type=TEXT, nullable=True)
